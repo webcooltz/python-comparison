c9d4d43028e2762f2c841f49bf2c2e081e535e9b
Tyler Tucker <webcooltz@duck.com>
2022-12-20 10:59:08 -0700

    Differing files
    Added support for finding identical and different files.

--- compare-directories.py
+++ compare-directories.py
@@ -15,22 +15,44 @@ def select_directory_2():
 
 
 def compare_directories():
+    # compares directories
     result = filecmp.dircmp(d1_text.get(), d2_text.get())
-    result.report()
-    result_common.set(result.common.__str__())
-    result_diff_files_list.set(result.diff_files.__str__())
+    # finds similarly-named files
+    result_matching_name_list.set(result.common.__str__())
+    # finds similarly-named files that have SAME contents
+    result_identical_list.set(result.same_files.__str__())
+    # finds similarly-named files that have DIFF contents
+    result_conflicting_list.set(result.diff_files.__str__())
+    # finds similarly-named directories
+    # result_matching_directory_list.set(result.common_dirs.__str__())
 
-    diff_result = result.diff_files
+    # ----- finds differing files -----
+    result_1 = result.left_list
+    result_2 = result.right_list
+    diff_array_1 = []
+    diff_array_2 = []
 
-    for file in diff_result:
+    for file in result_1:
+        if file not in result_2:
+            diff_array_1.append(file)
+
+    result_diff_files_list_1.set(diff_array_1.__str__())
+
+    for file in result_2:
+        if file not in result_1:
+            diff_array_2.append(file)
+
+    result_diff_files_list_2.set(diff_array_2.__str__())
+
+    conflicting_files = result.diff_files
+
+# reads each conflicting file and displays the contents
+    for file in conflicting_files:
         file_opener1 = open(d1_text.get() + "/" + file, "r")
         file_opener2 = open(d2_text.get() + "/" + file, "r")
 
         file_reader1 = file_opener1.read()
         file_reader2 = file_opener2.read()
-
-        # print("d1 " + file + ": " + file_opener1.read())
-        # print("d2 " + file + ": " + file_opener2.read())
 
         d1_result_diff_files.set(file_reader1)
         d2_result_diff_files.set(file_reader2)
@@ -38,14 +60,22 @@ def compare_directories():
         file_opener1.close()
         file_opener2.close()
 
-    # d1_result = result.left_list
-    # d2_result = result.right_list
-
 
 # establishes the main GUI window
 window = tk.Tk()
 window.geometry("500x300")
 window.title("Comparyson")
+
+# StringVars
+d1_text = tk.StringVar()
+d2_text = tk.StringVar()
+result_matching_name_list = tk.StringVar()
+result_conflicting_list = tk.StringVar()
+result_identical_list = tk.StringVar()
+result_diff_files_list_1 = tk.StringVar()
+result_diff_files_list_2 = tk.StringVar()
+d1_result_diff_files = tk.StringVar()
+d2_result_diff_files = tk.StringVar()
 
 # buttons to open directories
 open_button1 = ttk.Button(
@@ -60,49 +90,38 @@ open_button2 = ttk.Button(
     command=select_directory_2
 ).grid(row=1, column=2)
 
-# to change label strings
-d1_text = tk.StringVar()
-d2_text = tk.StringVar()
-result_common = tk.StringVar()
-result_diff_files_list = tk.StringVar()
-d1_result_diff_files = tk.StringVar()
-d2_result_diff_files = tk.StringVar()
-
-# labels for the entry boxes
+# labels for selected directories
 tk.Label(window, text="Directory 1:").grid(row=0)
 tk.Label(window, text="Directory 2:").grid(row=1)
-
-# d1 = ""
-# d2 = ""
 d1 = tk.Label(window, textvariable=d1_text).grid(row=0, column=1)
 d2 = tk.Label(window, textvariable=d2_text).grid(row=1, column=1)
 
-# entry boxes
-# d1 = tk.Entry(window).grid(row=0, column=1)
-# d2 = tk.Entry(window).grid(row=1, column=1)
-file_entry_array = []
-
-# labels for the answer fields
-tk.Label(window, text="Match:").grid(row=5)
-tk.Label(window, text="Conflicting Files:").grid(row=6)
-tk.Label(window, text="Diff Files:").grid(row=7)
-tk.Label(window, text="Errors:").grid(row=8)
-
-# button to call compare_directories()
+# call compare_directories()
 tk.Button(window, text='Compare', command=compare_directories).grid(row=3,
                                                                     column=1,
                                                                     sticky=tk.W,
                                                                     pady=4)
 
-# result labels
-match_answer_label = tk.Label(window, textvariable=result_common).grid(row=5, column=1)
-diff_answer_label = tk.Label(window, textvariable=result_diff_files_list).grid(row=6, column=1)
+# labels for the result fields
+tk.Label(window, text="Same file names:").grid(row=5)
+tk.Label(window, text="Identical files:").grid(row=6)
+tk.Label(window, text="Conflicting Files:").grid(row=7)
+tk.Label(window, text="Different Files (d1):").grid(row=8)
+tk.Label(window, text="Different Files (d2):").grid(row=9)
+# tk.Label(window, text="Errors:").grid(row=10)
 
-# file result labels
-tk.Label(window, text="d1 file:").grid(row=9, column=0)
-tk.Label(window, text="d2 file:").grid(row=9, column=1)
-d1_diff_file_label = tk.Label(window, textvariable=d1_result_diff_files).grid(row=10, column=0)
-d2_diff_file_label = tk.Label(window, textvariable=d2_result_diff_files).grid(row=10, column=1)
+# result fields
+match_answer_label = tk.Label(window, textvariable=result_matching_name_list).grid(row=5, column=1)
+identical_answer_label = tk.Label(window, textvariable=result_identical_list).grid(row=6, column=1)
+conflicting_files_label = tk.Label(window, textvariable=result_conflicting_list).grid(row=7, column=1)
+diff_files_label_1 = tk.Label(window, textvariable=result_diff_files_list_1).grid(row=8, column=1)
+diff_files_label_2 = tk.Label(window, textvariable=result_diff_files_list_2).grid(row=9, column=1)
+
+# printed file labels
+tk.Label(window, text="d1 file:").grid(row=10, column=0)
+tk.Label(window, text="d2 file:").grid(row=10, column=1)
+d1_diff_file_label = tk.Label(window, textvariable=d1_result_diff_files).grid(row=11, column=0)
+d2_diff_file_label = tk.Label(window, textvariable=d2_result_diff_files).grid(row=11, column=1)
 
 # create a notebook
 # notebook = ttk.Notebook(window).grid(row=12, column=0)
@@ -124,10 +143,10 @@ tk.mainloop()
 # D:/projects-coding/python/python-comparison/d1
 # D:/projects-coding/python/python-comparison/d2
 
-# TODO - Add directory-picker
+# TODO - Get different files and list them
 # TODO - Add tabs or buttons to show each file comparison
-# TODO - Get similarly named files that are identical and list them
 # TODO - Compare line-by-line and -- show the differences on the GUI
 # TODO - List subdirectories/ their files and compare those
 # TODO - Add error handling
 # TODO - Add limits to prevent memory leaks, crashes, etc.
+# TODO - Prevent from being run multiple times?
